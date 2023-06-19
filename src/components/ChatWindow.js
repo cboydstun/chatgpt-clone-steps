a8ff2e2f71b92e5e10ffaa7eaf21a27c3ff933b8
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2023-06-19 06:39:17 -0500

    rendering components

--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -8,10 +8,30 @@ export default function ChatWindow() {
   const [loading, setLoading] = useState(false)
   const [input, setInput] = useState('')
 
+  const handleSubmit = async (e) => { }
+
   return (
     <div>
-      <Message />
-      <LoadingIndicator />
+      {/* DISPLAY ALL MESSAGES */}
+      {messages.map((message, index) => (
+        <Message
+          key={index}
+          sender={message.sender}
+          text={message.text} />
+      ))}
+
+
+      {/* DISPLAY LOADING INDICATOR */}
+      {loading && <LoadingIndicator />}
+
+      {/* DISPLAY INPUT */}
+      <form onSubmit={handleSubmit} >
+        <input
+          type="text"
+          value={input}
+          onChange={e => setInput(e.target.value)} />
+        <button type="submit">Send</button>
+      </form>
     </div>
   )
 }
